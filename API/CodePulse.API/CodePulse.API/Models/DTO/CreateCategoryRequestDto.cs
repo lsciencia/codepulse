325455fced55b4938cecd3a5fd663727d21f9e4c
lsciencia <luissciencia.t.i@gmail.com>
2023-10-18 15:19:32 -0300

    building API using ASP.NET Code and VS

---
+++ API/CodePulse.API/CodePulse.API/Models/DTO/CreateCategoryRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CodePulse.API.Models.DTO
+{
+    public class CreateCategoryRequestDto
+    {
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+
+    }
+}
